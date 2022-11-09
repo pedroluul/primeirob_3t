10ec926fceb2404fbcfa82037203caf427ccc8ea
pedroluul <110116411+pedroluul@users.noreply.github.com>
2022-11-09 12:48:35 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,30 +1,50 @@
-let tabuada = 2;
+function total(){
+    let c = document.getElementById("capital").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    if(!Number(c)){
+        alert("O valor do capital deve ser um número.");
+        document.getElementById("capital").value = "";
+        document.getElementById("capital").focus();
+        return 
+    }
+    if(!Number(j)){
+        alert("O valor dos juros deve ser um número.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return 
+    }
+    if(!Number(t)){
+        alert("A quantidade de meses deve ser um número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return 
+    }
+    let m = 0;
+    for(let i = 1; i <= t; i++){
+        m = c * (1 + (j/100));
+        //document.write("Valor no mês " + i + " = " + m + "<br>");
+        c = m;
+    }
+    document.getElementById("total").innerHTML = m;
+    //document.write("Montante: " + m);
+}
 
-function escreva (){
-    document.write("<h1> Tabuada do " + tabuada + "</h1>")
-    document.write(tabuada + "x 1 =" + (tabuada*1)+ "<br>");
-    document.write(tabuada + "x 2 =" + (tabuada*2)+ "<br>");
-    document.write(tabuada + "x 3 =" + (tabuada*3)+ "<br>");
-    document.write(tabuada + "x 4 =" + (tabuada*4)+ "<br>");
-    document.write(tabuada + "x 5 =" + (tabuada*5)+ "<br>");
-    document.write(tabuada + "x 6 =" + (tabuada*6)+ "<br>");
-    document.write(tabuada + "x 7 =" + (tabuada*7)+ "<br>");
-    document.write(tabuada + "x 8 =" + (tabuada*8)+ "<br>");
-    document.write(tabuada + "x 9 =" + (tabuada*9)+ "<br>");
-    document.write(tabuada + "x 10 =" + (tabuada*10)+ "<br>");
-}
-let lista = ["jesus","spessato","duarte","anna","douglas","maria"]
-function mostralista(){
-document.write("tamanho da lista:"+ lista.length+"<br>");
-for(let i = 0; i < lista.length; i++){
-    document.write("professor: " + lista[i] + "<br>")
- }
-}
-function multiplica(){
-    for(let i = 1; i <= 10; i++) {
-    document.write ("o valor de i = " + i + "<br>");
-for(let j = i; j <= 10; j++){
-    document.write(i + " x " +j+" = " + (i*j)+ "<br>")
-}
-}
+function calculaRaiz(){
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+    //ax2 + bx + c = 0
+    let delta = (b*b) - (4*a*c);
+
+    if (delta < 0){
+        document.getElementById("raiz").innerHTML = "Não possui raiz real";
+    } else if (delta == 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById("raiz").innerHTML = "x1 = x2 = " + x1;
+    } else {
+        let x1 = ((-b) + Math.sqrt(delta))/(2*a);
+        let x2 = ((-b) - Math.sqrt(delta))/(2*a);
+        document.getElementById("raiz").innerHTML = "x1 = " + x1 + " x2 = " + x2;
+    }
 }
